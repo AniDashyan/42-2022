964db0fdb2bfd8716bbf87dfd4eb53d8ba363a3e
davitmartirosyan <phpencoding@gmail.com>
2023-01-11 14:44:36 +0400

    philo

---
+++ Philosophers/philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tumolabs <tumolabs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 11:53:04 by tumolabs          #+#    #+#             */
+/*   Updated: 2023/01/11 11:55:02 by tumolabs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
