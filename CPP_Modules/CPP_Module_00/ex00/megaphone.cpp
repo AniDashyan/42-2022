a81b1eea749ff98ccf0d181ad11b839638e9c86f
Ani <ani.dashyan77@gmail.com>
2023-07-17 15:35:27 +0400

    CPP Module 00 ex00 done

---
+++ CPP_Modules/CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; j < (int)strlen(argv[i]); j++)
+				std::cout << char(toupper(argv[i][j]));
+	}
+	return 0;
+}
