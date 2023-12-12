266863441b9dfca82e910922d5ae044419a25cb4
AniDashyan <45006450+AniDashyan@users.noreply.github.com>
2023-12-12 20:51:12 +0400

    CPP Module 08 finished

--- CPP_Modules/CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Modules/CPP_Module_08/ex02/MutantStack.hpp
@@ -6,10 +6,22 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
     public:
-        MutantStack();
-        MutantStack(const MutantStack& other);
-        MutantStack& operator=(const MutantStack& other);
-        ~MutantStack();
+        typedef typename std::stack<T>::container_type::iterator iterator;
+    public:
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+        MutantStack& operator=(const MutantStack& other) {
+            std::stack<T>::operator=(other);
+        }
+        ~MutantStack() {}
+
+        iterator begin() {
+            return (this->c.begin());
+        }
+
+        iterator end() {
+            return (this->c.end());
+        }
 };
 
 #endif
